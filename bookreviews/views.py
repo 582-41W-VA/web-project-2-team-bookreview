1d2c3e239f539616d09aeddf0d74b045babb8c6e
HHHuser1 <wohooo@yahoo.com>
2024-03-26 12:11:24 -0400

    Correct views.py code

--- bookreviews/views.py
+++ bookreviews/views.py
@@ -20,77 +20,6 @@ from django.core.cache import cache
 
 
 
-
-# def index(request):
-#     books = Book.objects.all()
-#     return render(request, 'index.html', {'books': books})
-
-
-# def book_detail(request, book_id):
-#     book = Book.objects.get(id=book_id)
-#     reviews = Review.objects.filter(book=book)
-#     return render(request, 'book_detail.html', {'book': book, 'reviews': reviews})
-
-
-# @login_required
-# def leave_review(request, book_id):
-#     if not request.user.is_authenticated:
-#         login_url = reverse('login')
-#         login_link = f'<a class="login-link" href="{login_url}">Log in</a>'
-#         register_link = f'<a class="register-link" href="{reverse("register")}">Register</a>'
-#         messages.info(request, f'You need to log in to leave a review. Please {login_link} or {register_link}.')
-#         return redirect('login')
-#     else:
-#         book = get_object_or_404(Book, id=book_id)
-#         if request.method == 'POST':
-#             form = ReviewForm(request.POST)
-#             if form.is_valid():
-#                 review = form.save(commit=False)
-#                 review.book = book  # Assign the current book to the review
-#                 review.user = request.user  # Assign the logged-in user to the review
-#                 review.save()
-#                 messages.success(request, 'Your review has been submitted successfully.')
-#                 return redirect('book_detail', book_id=book.id)  # Redirect to the book detail page
-#         else:
-#             form = ReviewForm()
-#         return render(request, 'leave_review.html', {'form': form, 'book': book})
-    
-
-
-
-
-# def edit_review(request, review_id):
-#     review = get_object_or_404(Review, id=review_id)
-#     if review.user == request.user:  # Check if the logged-in user owns the review
-#         if request.method == 'POST':
-#             form = ReviewForm(request.POST, instance=review)
-#             if form.is_valid():
-#                 form.save()
-#                 messages.success(request, 'Your review has been edited successfully.')
-#                 return redirect('book_detail', book_id=review.book.id)  # Redirect to user's reviews page
-#         else:
-#             form = ReviewForm(instance=review)
-#         return render(request, 'edit_review.html', {'form': form, 'book': review.book})
-#     else:
-#         return HttpResponseForbidden("You are not authorized to edit this review.")
-
-
-
-
-# def delete_review(request, review_id):
-#     review = get_object_or_404(Review, id=review_id)
-#     if review.user == request.user:  # Check if the logged-in user owns the review
-#         if request.method == 'POST' and request.POST.get('confirm_delete'):
-#             review.delete()
-#             messages.success(request, 'Your review has been deleted successfully.')
-#             return redirect('book_detail', book_id=review.book.id)  # Redirect to book detail page after deletion
-#         else:
-#             return render(request, 'confirm_delete_review.html', {'review': review})
-#     else:
-#         return HttpResponseForbidden("You are not authorized to delete this review.")
-
-
-
 def edit_review(request, review_id):
     review = get_object_or_404(Review, id=review_id)
     if review.user == request.user:  # Check if the logged-in user owns the review
@@ -115,28 +44,6 @@ def edit_review(request, review_id):
     else:
         return HttpResponseForbidden("You are not authorized to edit this review.")
 
-
-# def delete_review(request, review_id):
-#     review = get_object_or_404(Review, id=review_id)
-#     if review.user == request.user:  # Check if the logged-in user owns the review
-#         if request.method == 'POST' and request.POST.get('confirm_delete'):
-#             book_id = review.book_id  # Store the book ID before deletion
-#             review.delete()
-#             messages.success(request, 'Your review has been deleted successfully.')
-#             return redirect('book_detail', book_id=book_id)  # Redirect to book detail page after deletion
-#         else:
-#             # Pass the review and relevant book data along to the template
-#             book_data = {
-#                 "id": review.book_id,
-#                 "title": review.book_title,
-#                 "author": review.book_author,
-#                 "description": review.book_description,
-#                 "category": review.book_category,
-#                 "image": review.book_image,
-#             }
-#             return render(request, 'confirm_delete_review.html', {'review': review})
-#     else:
-#         return HttpResponseForbidden("You are not authorized to delete this review.")
 
 
 
@@ -195,69 +102,6 @@ def register(request):
     else:
         form = RegistrationForm()
     return render(request, 'register.html', {'form': form})
-
-
-# @login_required
-# def my_reviews(request):
-#     user_reviews = Review.objects.filter(user=request.user)
-#     return render(request, 'my_reviews.html', {'user_reviews': user_reviews})
-
-
-# @login_required
-# def my_reviews(request):
-#     user_reviews = Review.objects.filter(user=request.user)
-#     reviews_with_books = []
-
-#     # Iterate over each review to fetch the corresponding book data from the API
-#     for review in user_reviews:
-#         # Fetch book data from the API using the book ID stored in the review
-#         google_book_api = f"https://www.googleapis.com/books/v1/volumes/{review.book_id}"
-#         response = requests.get(google_book_api).json()
-
-#         if "error" not in response:
-#             info = response["volumeInfo"]
-#             book_data = {
-#                 "title": info.get("title", "Title Not Available"),
-#                 "author": info.get("authors", ["Author Not Available"])[0],
-#                 "description": info.get("description", "No Description"),
-#                 "category": info.get("categories", ["No Categories"])[0],
-#                 "image": info.get("imageLinks", {}).get("thumbnail", "https://islandpress.org/files/default_book_cover_2015.jpg"),
-#             }
-
-#             # Append the review along with the fetched book data to the list
-#             reviews_with_books.append({'review': review, 'book': book_data})
-
-#     return render(request, 'my_reviews.html', {'reviews_with_books': reviews_with_books})
-
-
-
-
-
-# @login_required
-# def my_reviews(request):
-#     # Check if the reviews_with_books data is already cached
-#     reviews_with_books = cache.get('user_reviews_with_books')
-    
-#     # If not cached, retrieve the data and cache it
-#     if not reviews_with_books:
-#         # Retrieve the user's reviews
-#         user_reviews = Review.objects.filter(user=request.user)
-#         # Fetch additional book data based on the book ID in each review
-#         reviews_with_books = []
-#         for review in user_reviews:
-#             # Fetch book data from API or any other source based on review.book_id
-#             book_data = {
-#                 "title": "Book Title",
-#                 "author": "Book Author",
-#                 # Add other book data as needed
-#             }
-#             reviews_with_books.append({"review": review, "book": book_data})
-        
-#         # Cache the data for future requests
-#         cache.set('user_reviews_with_books', reviews_with_books, timeout=10)  # Cache for 15 minutes
-
-#     return render(request, 'my_reviews.html', {'reviews_with_books': reviews_with_books})
-
 
 
 
